4932bca81f00354108c4f57f1d02f115d3ed5e76
Richard Barrett-Small <rickbs@gmail.com>
2014-04-20 18:45:19 +0100

    Validating postcode against much shorter list

--- constructive-postcodes.php
+++ constructive-postcodes.php
@@ -73,17 +73,25 @@ function cpc_validate_text_postcode($result, $tag) {
 		// We have a postcode field here
 		$value = $_POST[$name];
 		$pc_slug = slugify_postcode($value);
-		$confirmed_pc = $wpdb->get_row(
-			$wpdb->prepare(
-				"
-					SELECT * FROM $cpc_table_name
-					WHERE postcode_slug = %s
-				",
-				$pc_slug
-		) );
-
-		if (null === $confirmed_pc) {
-			$result['reason'][$name] = 'Not a London Postcode';
+		# Check for a minimum length (postcodes must be > 5 chars)
+		if (strlen($pc_slug) > 4) {
+			# Chop last character off to check against shorter list
+			$pc_slug = substr($pc_slug, 0, -1);
+			# check if there's a match
+			$confirmed_pc = $wpdb->get_row(
+				$wpdb->prepare(
+					"
+						SELECT * FROM $cpc_table_name
+						WHERE postcode_slug LIKE %s
+					",
+					like_escape($pc_slug) . '%'
+			) );
+			if (null === $confirmed_pc) {
+				$result['reason'][$name] = 'Not a London postcode';
+				$result['valid'] = false;
+			}
+		} else {
+			$result['reason'][$name] = 'Postcode too short';
 			$result['valid'] = false;
 		}
 	}
